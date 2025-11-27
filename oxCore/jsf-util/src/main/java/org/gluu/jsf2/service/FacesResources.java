33f1bcc10da073bbea251e0f08ca9891559e37c6
Yuriy <Yuriy.Movchan@gmail.com>
2025-11-27 21:27:17 +0300

    Revert "feat: replace ResourceHandler with ResourceHandlerWrapper to conform"
    This reverts commit 36b5c3027a2e03711b5288dbb926565217a2c7e9.

--- oxCore/jsf-util/src/main/java/org/gluu/jsf2/service/FacesResources.java
+++ oxCore/jsf-util/src/main/java/org/gluu/jsf2/service/FacesResources.java
@@ -3,8 +3,8 @@ package org.gluu.jsf2.service;
 import jakarta.enterprise.context.Dependent;
 import jakarta.enterprise.inject.Produces;
 import jakarta.faces.application.ViewHandler;
+import jakarta.faces.context.ExternalContext;
 import jakarta.faces.context.FacesContext;
-import jakarta.inject.Inject;
 
 /**
  * @author Yuriy Movchan
@@ -13,12 +13,27 @@ import jakarta.inject.Inject;
 @Dependent
 public class FacesResources {
 
-    @Inject
-    private FacesContext facesContext;
+    @Produces
+    @Dependent
+    public FacesContext getFacesContext() {
+        return FacesContext.getCurrentInstance();
+    }
+
+    @Produces
+    @Dependent
+    public ExternalContext getExternalContext() {
+        FacesContext facesContext = getFacesContext();
+        if (facesContext != null) {
+            return facesContext.getExternalContext();
+        }
+
+        return null;
+    }
 
     @Produces
     @Dependent
     public ViewHandler getViewHandler() {
+        FacesContext facesContext = getFacesContext();
         if (facesContext != null) {
             return facesContext.getApplication().getViewHandler();
         }
